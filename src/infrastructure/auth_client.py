0ecab41fdd67ec26f47048ec128c76b5829d52ed
Sidney Alex <silvercenturion2@gmail.com>
2024-11-01 17:30:09 -0300

    fix auth

--- src/infrastructure/auth_client.py
+++ src/infrastructure/auth_client.py
@@ -1,33 +1,42 @@
 import base64
+
 import httpx
+from pydantic import BaseModel
+
 from src.domain.auth import AuthCredentials
+
+
+class AuthResponse(BaseModel):
+    access_token: str
+
 
 class AuthClient:
     BASE_URL = "https://api.strateegia.digital/users/v1"
 
-    async def login(self, credentials: AuthCredentials):
+    async def login(self, credentials: AuthCredentials) -> AuthResponse:
         basic_auth = base64.b64encode(
             f"{credentials.username}:{credentials.password}".encode()
         ).decode()
-        
+
         headers = {
             "Content-Type": "application/json",
             "Authorization": f"Basic {basic_auth}",
             "User-Agent": "Orimole/1.0"
         }
-        
+
         payload = {
             "keepConnected": True
         }
-        
+
         async with httpx.AsyncClient() as client:
             response = await client.post(
                 f"{self.BASE_URL}/auth/signin",
                 json=payload,
                 headers=headers
             )
-            
+
             if response.status_code == 200:
-                return response.json().get("access_token")
+                return AuthResponse(access_token=response.json()["access_token"])
             else:
-                raise Exception(f"Failed to authenticate with Strateegia: {response.text}")
+                raise Exception(
+                    f"Failed to authenticate with Strateegia: {response.text}")
