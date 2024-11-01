215b37d062428038a9852cb880efc0f2086922c1
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-11-01 17:16:39 -0300

    update auth_client

--- src/infrastructure/auth_client.py
+++ src/infrastructure/auth_client.py
@@ -1,3 +1,4 @@
+import base64
 import httpx
 from src.domain.auth import AuthCredentials
 
@@ -5,16 +6,27 @@ class AuthClient:
     BASE_URL = "https://api.strateegia.digital/users/v1"
 
     async def login(self, credentials: AuthCredentials):
+        basic_auth = base64.b64encode(
+            f"{credentials.username}:{credentials.password}".encode()
+        ).decode()
+        
         headers = {
             "Content-Type": "application/json",
-            "User-Agent": "Orimole/1.0"  # Opcional, mas pode ser necessário
+            "Authorization": f"Basic {basic_auth}",
+            "User-Agent": "Orimole/1.0"
         }
+        
+        payload = {
+            "keepConnected": True
+        }
+        
         async with httpx.AsyncClient() as client:
             response = await client.post(
                 f"{self.BASE_URL}/auth/signin",
-                json={"username": credentials.username, "password": credentials.password},
+                json=payload,
                 headers=headers
             )
+            
             if response.status_code == 200:
                 return response.json().get("access_token")
             else:
